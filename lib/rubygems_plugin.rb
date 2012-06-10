d131214b3957fbc010faa6a1b5d7cb44107e8c06
Justin Baker <azolo1089@gmail.com>
2012-06-09 20:50:37 -0500

    Delay requires to pre_install hook

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,9 +1,9 @@
 require "rubygems/command_manager"
-require 'exefy'
-require 'exefy/gem_install_stub'
 
 Gem::CommandManager.instance.register_command :exefy
 
 Gem.pre_install do |installer|
+  require 'exefy'
+  require 'exefy/gem_install_stub'
   installer.extend(Exefy::StubOverride)
 end
