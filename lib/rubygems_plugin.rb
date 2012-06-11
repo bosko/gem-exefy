a3f977478c63c0cf5339998f3c1e7ebdbcacd8f7
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2012-06-11 11:23:47 +0200

    Remove unnecessary require

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -3,7 +3,6 @@ require "rubygems/command_manager"
 Gem::CommandManager.instance.register_command :exefy
 
 Gem.pre_install do |installer|
-  require 'exefy'
   require 'exefy/gem_install_stub'
   installer.extend(Exefy::StubOverride)
 end
